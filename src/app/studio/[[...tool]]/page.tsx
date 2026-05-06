efb79b29eece04a12c844f22c684857e71b80d95
Natchanon002 <hutza00711@gmail.com>
2026-05-06 14:53:22 +0700

    fix: dynamic import studio to prevent SSR window error

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../../sanity.config";
+import dynamic from "next/dynamic";
+
+const Studio = dynamic(
+  () =>
+    import("next-sanity/studio").then((mod) => {
+      const { NextStudio } = mod;
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const config = require("../../../../sanity.config").default;
+      return function StudioInner() {
+        return <NextStudio config={config} basePath="/studio" />;
+      };
+    }),
+  { ssr: false, loading: () => <div style={{ height: "100vh", display: "grid", placeItems: "center", background: "#101112", color: "#fff", fontFamily: "sans-serif" }}>Loading Studio...</div> }
+);
 
 export default function StudioPage() {
-  return <NextStudio config={config} basePath="/studio" />;
+  return <Studio />;
 }
